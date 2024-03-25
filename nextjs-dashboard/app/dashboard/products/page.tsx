ebdc5ec81d551cd4ef507667a98c4944f8387309
Welcome ! <usmanubaid9@gmail.com>
2024-03-25 06:26:01 +0100

    feat: design product page

--- nextjs-dashboard/app/dashboard/products/page.tsx
+++ nextjs-dashboard/app/dashboard/products/page.tsx
@@ -1,7 +1,65 @@
+import Image from "next/image";
+import Link from "next/link";
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import styles from "@/app/ui/dashboard/products/products.module.css";
+import Search from "@/app/ui/dashboard/search/search";
 
 const ProductsPage = () => {
-  return <div className={styles.container}>ProductsPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search a product" />
+        <Link href="/dashboard/products/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Title</td>
+            <td>Description</td>
+            <td>Price</td>
+            <td>Created at</td>
+            <td>Stock</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.product}>
+                <Image
+                  src="/noproduct.jpg"
+                  alt=""
+                  width={30}
+                  height={30}
+                  className={styles.productImage}
+                />
+                Iphone
+              </div>
+            </td>
+            <td>Iphone 13 Ultra pro max for sale</td>
+            <td>$675</td>
+            <td>Feb 22, 2024</td>
+            <td>24</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
+  );
 };
 
 export default ProductsPage;
--- nextjs-dashboard/app/dashboard/users/page.tsx
+++ nextjs-dashboard/app/dashboard/users/page.tsx
@@ -1,8 +1,8 @@
+import Link from "next/link";
+import Image from "next/image";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
-import Image from "next/image";
-import Link from "next/link";
 
 const UsersPage = () => {
   return (
