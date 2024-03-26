7f360e5914043bb65e810c4995cfefa7904ab536
Welcome ! <usmanubaid9@gmail.com>
2024-03-26 06:05:02 +0100

    feat: design login page

--- nextjs-dashboard/app/login/page.tsx
+++ nextjs-dashboard/app/login/page.tsx
@@ -1,5 +1,16 @@
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <h1>Login</h1>
+        <input type="email" name="email" placeholder="email" />
+        <input type="password" name="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
