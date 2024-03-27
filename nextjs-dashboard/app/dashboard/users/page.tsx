d210ce19767b2604fb4e691ae78edc5c7ac7f736
Welcome ! <usmanubaid9@gmail.com>
2024-03-27 15:18:26 +0100

    feat: search functionality

--- nextjs-dashboard/app/dashboard/users/page.tsx
+++ nextjs-dashboard/app/dashboard/users/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import Pagination from "@/app/ui/dashboard/pagination/pagination";
 import Search from "@/app/ui/dashboard/search/search";
 import styles from "@/app/ui/dashboard/users/users.module.css";
 import { fetchUsers } from "@/app/lib/data";
 
-const UsersPage = async () => {
-  const users = await fetchUsers();
+type UsersPageProps = {
+  searchParams: {
+    q?: string;
+  };
+};
+
+const UsersPage = async ({ searchParams }: UsersPageProps) => {
+  const q = searchParams?.q || "";
+
+  const users = await fetchUsers(q);
   return (
     <div className={styles.container}>
       <div className={styles.top}>
--- nextjs-dashboard/app/ui/dashboard/search/search.tsx
+++ nextjs-dashboard/app/ui/dashboard/search/search.tsx
@@ -1,15 +1,40 @@
+"use client";
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 type SearchProps = {
   placeholder: string;
 };
 
 const Search = ({ placeholder }: SearchProps) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const handleSearch = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const params = new URLSearchParams(searchParams);
+      if (e.target.value) {
+        e.target.value.length > 2 && params.set("q", e.target.value);
+      } else {
+        params.delete("q");
+      }
+      replace(`${pathname}?${params}`);
+    },
+    300
+  );
+
   return (
     <div className={styles.container}>
       <MdSearch size={20} />
-      <input type="text" placeholder={placeholder} className={styles.input} />
+      <input
+        type="text"
+        placeholder={placeholder}
+        className={styles.input}
+        onChange={handleSearch}
+      />
     </div>
   );
 };
