f353dbe3c392bf2ad00c633bac5f385fcf6c93db
Welcome ! <usmanubaid9@gmail.com>
2024-03-28 14:47:27 +0100

    feat: hash password

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -1,19 +1,23 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { User } from "./models";
+import { Product, User } from "./models";
 import { connectToDB } from "./utils";
 import { redirect } from "next/navigation";
+import bcrypt from "bcrypt";
 
 export const addUser = async (formData: FormData) => {
   const { username, email, password, phone, isActive, address, isAdmin } =
     Object.fromEntries(formData);
 
   connectToDB();
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password.toString(), salt);
+
   const newUser = new User({
     username,
     email,
-    password,
+    password: hashedPassword,
     phone,
     isActive,
     address,
@@ -25,3 +29,5 @@ export const addUser = async (formData: FormData) => {
   revalidatePath("/dashboard/users");
   redirect("/dashboard/users");
 };
+
+
