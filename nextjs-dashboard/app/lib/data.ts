6f546f3dc08940066aab745520abb71b7062a73b
Welcome ! <usmanubaid9@gmail.com>
2024-03-28 06:07:57 +0100

    feat: fetch products

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -1,14 +1,42 @@
-import { User } from "./models";
+import { Product, User } from "./models";
 import { connectToDB } from "./utils";
 
-export const fetchUsers = async (q: string) => {
+export const fetchUsers = async (q: string, page: string) => {
   const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
   try {
     connectToDB();
-    const users = await User.find({ username: { $regex: regex } });
-    return users;
+    const countUsers = await User.find({
+      username: { $regex: regex },
+    }).countDocuments();
+
+    const users = await User.find({ username: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (parseInt(page) - 1));
+
+    return { countUsers, users };
   } catch (error) {
     console.log(error);
     throw new Error("Failed to fetch users!");
+  }
+};
+
+export const fetchProducts = async (q: string, page: string) => {
+  const regex = new RegExp(q, "i");
+  const ITEM_PER_PAGE = 2;
+  try {
+    connectToDB();
+    const countProducts = await Product.find({
+      title: { $regex: regex },
+    }).countDocuments();
+
+    const products = await Product.find({ title: { $regex: regex } })
+      .limit(ITEM_PER_PAGE)
+      .skip(ITEM_PER_PAGE * (parseInt(page) - 1));
+
+    return { countProducts, products };
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to fetch products!");
   }
 };
