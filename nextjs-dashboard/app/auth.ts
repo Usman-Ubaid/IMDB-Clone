91efb7d63326f43c4d3989a2569f8e8f27ed4c05
Welcome ! <usmanubaid9@gmail.com>
2024-03-30 06:03:58 +0100

    feat: authentication implementation

---
+++ nextjs-dashboard/app/auth.ts
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { authConfig } from "../auth.config";
+import bcrypt from "bcrypt";
+import { connectToDB } from "./lib/utils";
+import { User } from "./lib/models";
+
+const getUser = async (credentials: {
+  username?: string;
+  password?: string;
+}) => {
+  try {
+    connectToDB();
+    if (!credentials.username || !credentials.password) {
+      throw new Error("Fill all the fields");
+    }
+    const user = await User.findOne({ username: credentials.username });
+    if (!user) throw new Error("Wrong Credentials!");
+    const isPasswordCorrect = await bcrypt.compare(
+      credentials.password,
+      user.password
+    );
+    if (!isPasswordCorrect) throw new Error("Wrong Credentials!");
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to login");
+  }
+};
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials: { username?: string; password?: string }) {
+        try {
+          const user = await getUser(credentials);
+          return user;
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+});
